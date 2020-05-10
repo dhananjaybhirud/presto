a0d950d7171a74b0a14bdac74309e7001dc4bfa2
dhananjaybhirud <dhananjay.b@mobinexttech.com>
2020-05-10 10:52:07 +0530

    - details page

--- src/app/components/popular/popular.component.ts
+++ src/app/components/popular/popular.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -12,9 +12,28 @@ export class PopularComponent implements OnInit {
     title2: '',
     align: ''
   }
+  itemsPerSlide;
+  innerWidth:number ;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth > 1200){
+      this.itemsPerSlide = 4;
+    } else if (  this.innerWidth <= 1200 && this.innerWidth >= 768 ){
+      this.itemsPerSlide = 3;
+    } else if(this.innerWidth >= 575 && this.innerWidth <= 767){
+      this.itemsPerSlide = 2;
+    } else {
+      this.itemsPerSlide = 1;
+    }
+
+    // console.log(this.innerWidth);
     if(this.router.url === '/details' ){
       this.temp={
         title2: 'ALSO LIKE',
